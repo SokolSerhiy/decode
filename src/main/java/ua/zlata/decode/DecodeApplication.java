1d4aff4fec6b5d7b03ff81e6748d8eb0a207d8ab
Serhii <sokol.serhij@gmail.com>
2018-12-03 01:51:27 +0300

    Add CORS for front end

--- src/main/java/ua/zlata/decode/DecodeApplication.java
+++ src/main/java/ua/zlata/decode/DecodeApplication.java
@@ -2,6 +2,7 @@ package ua.zlata.decode;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.CorsRegistry;
 import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 import org.springframework.web.reactive.result.method.annotation.ArgumentResolverConfigurer;
@@ -18,5 +19,13 @@ public class DecodeApplication implements WebFluxConfigurer {
     @Override
     public void configureArgumentResolvers(ArgumentResolverConfigurer configurer) {
         configurer.addCustomResolver(new OperationsAnnotationHandlerMethodArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://zlata-decode.herokuapp.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .maxAge(3600);
     }
 }
